6f8cef8ad700c67dd7fd09f9c6fff28ae7f9e9cc
Emmanuel NGBAME <ngbamedistingue@yahoo.com>
2024-09-24 14:33:35 +0200

    set up projet architecture client/server, base classes, database connection

---
+++ Dnk.Connect.App/BaseLibrary/DTOs/Register.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibrary.DTOs
+{
+	public class Register : AccountBase
+	{
+		[Required]
+		[MinLength(5)]
+		[MaxLength(10)]
+		public string? Fullname { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare(nameof(Password))]
+		public string? ConfirmPassword { get; set; }
+	}
+}
