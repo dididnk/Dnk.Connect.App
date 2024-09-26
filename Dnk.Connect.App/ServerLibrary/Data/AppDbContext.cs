933328676c544c19b7e36b1e8c6e27453ad48143
Emmanuel NGBAME <ngbamedistingue@yahoo.com>
2024-09-26 09:39:14 +0200

    update database connection string

--- Dnk.Connect.App/ServerLibrary/Data/AppDbContext.cs
+++ Dnk.Connect.App/ServerLibrary/Data/AppDbContext.cs
@@ -6,7 +6,12 @@ namespace ServerLibrary.Data
 	public class AppDbContext : DbContext
 	{
 
-		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			: base(options)
+		{
+		}
+
+		public AppDbContext() : base()
 		{
 		}
 
